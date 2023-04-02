512734562b13eba4c00919aa13222992635f1757
martinivelinov <124716132+martinivelinov@users.noreply.github.com>
2023-04-02 16:20:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll('button');
+
+
+
+
+
+
+
+
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
+
+function addToCart(event) {
+  const button = event.target;
+  const product = button.parentElement;
+  const productTitle = product.querySelector('h3').textContent;
+  const productPrice = product.querySelector('p').textContent;
+  const cartItems = document.querySelector('#cart ul');
+  const cartItemNames = cartItems.querySelectorAll('h3');
+  const cartItem = document.createElement('li');
+  const cartItemImage = document.createElement('img');
+  const cartItemTitle = document.createElement('h3');
+  const cartItemPrice = document.createElement('p');
+
+  cartItemImage.src = product.querySelector('img').src;
+  cartItemTitle.textContent = productTitle;
+  cartItemPrice.textContent = productPrice;
+
+  cartItem.appendChild(cartItemImage);
+  cartItem.appendChild(cartItemTitle);
+  cartItem.appendChild(cartItemPrice);
+
+  cartItems.appendChild(cartItem);
+  
+
+  }
